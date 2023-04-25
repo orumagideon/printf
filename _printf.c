cca77b6bc1ac19745ac65b35423fc6fe82deb2d4
orumagideon <orumagideon535@gmail.com>
2023-04-25 06:09:55 -0700

    update

--- _printf.c
+++ _printf.c
@@ -4,7 +4,7 @@ void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - The printf function
- * @format:The format used.
+ * @format: format.
  * Return: The printed chars.
  */
 int _printf(const char *format, ...)
