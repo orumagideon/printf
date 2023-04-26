d2963bf2bbc100f860270f612e055fa196c7967f
toxicfrank <ngugifrancis930@gmail.com>
2023-04-26 02:49:42 +0100

    alx

--- printf.c
+++ printf.c
@@ -74,14 +74,16 @@ int _printf(const char *format, ...)
 	int ret;
 
 	if (format == NULL)
+    {
 		return (-1);
+    }
 	output = init_buffer();
 	if (output == NULL)
+    {
 		return (-1);
-
+    }
 	va_start(args, format);
 
 	ret = run_printf(format, args, output);
-
 	return (ret);
 }
